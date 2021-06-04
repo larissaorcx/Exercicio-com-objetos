cfd39488cdf2e452f31952242163838b12f39d1b
Ingrid Larissa <larissaorcx@gmail.com>
2021-06-03 23:32:51 -0300

    inicio do programa

---
+++ Notas.js
@@ -0,0 +1,13 @@
+/*1. Escreva um programa que gera notas aleatórias de uma classe, e aponto quais alunos passaram, e quais precisam realizar a prova de recuperação
+*/ 
+
+let alunosClasse = Number(prompt("Digite o número de alunos na classe"))
+
+
+function creatAluno(alunosClasse) {
+    classe = []
+    for(let i = 0; i < classe; i++){
+        aluno = []
+        classe.push(aluno)
+    }
+}
