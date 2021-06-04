8591db82e70107533582d9e85efaa5456bf73594
Ingrid Larissa <larissaorcx@gmail.com>
2021-06-04 08:53:27 -0300

    corrigi a estrutura de pensamento, e transformei em algo mais simples

--- Notas.js
+++ Notas.js
@@ -1,6 +1,8 @@
 /*1. Escreva um programa que gera notas aleatórias de uma classe, e aponto quais alunos passaram, e quais precisam realizar a prova de recuperação
 */ 
 
+// um objeto que recebe os dados de uma classe (nota, média, desvio padrao).
+
 let alunosClasse = Number(prompt("Digite o número de alunos na classe"))
 
 
