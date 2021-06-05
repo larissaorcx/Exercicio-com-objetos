21d82a29f2221b96cc0e93bd91ba0fd1c2ec6111
Ingrid Larissa <larissaorcx@gmail.com>
2021-06-04 22:18:37 -0300

    Quase pronto

--- Notas.js
+++ Notas.js
@@ -1,22 +1,20 @@
 /*1. Escreva um programa que gera notas aleatórias de uma classe, e aponto quais alunos passaram, e quais precisam realizar a prova de recuperação
 */ 
 
-// um objeto que recebe os dados de uma classe (nota, média, desvio padrao).
-
 let alunosClasse = Number(prompt("Digite o número de alunos na classe"))
 
 
 function notasDaSala(alunosClasse) {
-    notas = []
+    let notas = []
     for(let i = 0; i < alunosClasse; i++){
-        nota = Math.random()
+        let nota = Math.random()
         notas.push(nota)
     }
     return(notas)
 }
 
 function media(notas, alunosClasse) {
-    soma = 0
+    let soma = 0
     for(let i = 0; i < alunosClasse; i++){
         soma += notas[i]
     }
@@ -25,12 +23,30 @@ function media(notas, alunosClasse) {
     return media
 }
 
+function alunosAprovados(notas, alunosClasse) {
+    aprovado = []
+    for(let i = 0; i < alunosClasse; i++){
+        if(notas[i] >= 5){
+            aprovado.push(notas)
+        }
+    }
+    return aprovado
+}
 
+function alunosReprovados(notas, alunosClasse) {
+    reprovados = []
+    for(let i = 0; i < alunosClasse; i++){
+        if(notas[i] < 5){
+            reprovados.push(notas)
+        }
+    }
+    return aprovado
+}
 
 let teacher = {
     notas: notasDaSala(alunosClasse),
     media: media(notas, alunosClasse),
-    aprovados: alunosAprovado(),
-    reprovados: alunosReprovados()
+    aprovados: alunosAprovado(notas, alunosClasse),
+    reprovados: alunosReprovados(notas, alunosClasse)
 
 }
